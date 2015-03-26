7a42b878290edcdf0b024867c615a34af7202cc6
Nathanael Ama <nathanael.ama1@gmail.com>
2015-03-26 13:37:57 +0200

    Async Methods to not crash main threads

--- app/src/main/java/com/markana/yamba/StatusActivity.java
+++ app/src/main/java/com/markana/yamba/StatusActivity.java
@@ -1,5 +1,6 @@
 package com.markana.yamba;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -9,8 +10,10 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import winterwell.jtwitter.Twitter;
+import winterwell.jtwitter.TwitterException;
 
 
 public class StatusActivity extends ActionBarActivity implements OnClickListener{
@@ -34,6 +37,43 @@ public class StatusActivity extends ActionBarActivity implements OnClickListener
 
         twitter = new Twitter("student", "password");
         twitter.setAPIRootUrl("http://yamba.marakana.com/api");
+    }
+
+    //Asynchronously posts to twitter
+    class PostToTwitter extends AsyncTask<String, Integer, String> {
+        //Called to initiate the background activity
+
+        @Override
+        protected String doInBackground(String... statuses) {
+            try {
+                winterwell.jtwitter.Status status = twitter.updateStatus(statuses[0]);
+                return status.text;
+            } catch (TwitterException e) {
+                Log.e(TAG, e.toString());
+                e.printStackTrace();
+                return "Failed to post";
+            }
+        }
+
+        //Called when there's a status to be updated
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            //Not used in this case
+        }
+
+        //Called once the background activity has completed
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG).show();
+        }
+
+        //Called when button is clicked
+        public void onClick(View view) {
+            String status = editText.getText().toString();
+            new PostToTwitter().execute(status);
+            Log.d(TAG, "onClicked");
+        }
     }
 
     // Called when button is clicked
